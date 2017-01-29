2903df74b3ebf6e50662ce5e6d6db64c6cfbbb20
Simon Müller <info@muon-stat.com>
2017-01-29 01:44:51 +0100

    Create AVFScoring.scala

---
+++ AVFScoring.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{udf, lit, col}
+
+object AVFOutlier {
+  def computeScore(df: DataFrame): DataFrame = {
+    // calculate frequency table for each columns saved as a map of map
+    val ft = frequencyMap(df)
+
+    // calculate score
+    val ftMap = ftCreateLookup(ft)
+    val dfCols = df.columns
+    df.withColumn("score", dfCols.map(x => ftMap(col(x), lit(x))).reduce((c1, c2) => c1 + c2))
+  }
+
+  // https://github.com/spirom/LearningSpark/blob/master/src/main/scala/dataframe/UDF.scala
+  def ftCreateLookup(m: Map[String, Map[Int, Int]])() = {
+    val f: (Int, String) => Int = {(v, c) => m(c).getOrElse(v, "0").toString.toInt}
+    udf(f)
+  }
+
+  def frequencyMap(data: DataFrame): Map[String, Map[Int, Int]] = {
+    def f(ct: Map[String, Map[Int, Int]], cols: Array[String]): Map[String, Map[Int, Int]] = cols match {
+      case cols if cols.length == 0 => ct
+      case _ =>
+        val col = cols.head
+        f(ct + (col -> data.groupBy(col).sum(col).collect.map(x => x.getInt(0) -> x.getAs[Int](1)).toMap), cols.tail)
+    }
+    val ct: Map[String, Map[Int, Int]] = Map()
+    f(ct, data.columns)
+  }
+}
